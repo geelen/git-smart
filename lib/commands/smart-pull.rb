b338c4cead241c9f7bdc81569ab67ead6946c399
Glen Maddern <glenmaddern@gmail.com>
2010-12-30 17:42:42 +1100

    Expanded the smart-pull, but the number of execution paths are increasing, so I need to start writing tests

--- lib/commands/smart-pull.rb
+++ lib/commands/smart-pull.rb
@@ -1,30 +1,56 @@
 GitSmart.register 'smart-pull' do |repo, args|
+  p repo
+  # Let's begin!
   branch = repo.current_branch
-
   start "Starting: smart-pull on branch '#{branch}'"
+
+  # Let's not have any arguments, fellas.
   warn "Ignoring arguments: #{args.inspect}" if !args.empty?
 
+  # Try grabbing the tracking remote from the config. If it doesn't exist,
+  # notify the user and choose 'origin'
   tracking_remote = repo.tracking_remote ||
     note("No tracking remote configured, assuming 'origin'") ||
     'origin'
 
-#  puts_with_done("Fetching '#{remote}'") { repo.fetch(remote) }
+  # Fetch the remote. This pulls down all new commits from the server, not just our branch,
+  # but generally that's a good thing. This is the only communication we need to do with the server.
+#  puts_with_done("Fetching '#{tracking_remote}'") { repo.fetch(tracking_remote) }
 
+  # Try grabbing the tracking branch from the config. If it doesn't exist,
+  # notify the user and choose the branch of the same name
   tracking_branch = repo.tracking_branch ||
     note("No tracking branch configured, assuming '#{branch}'") ||
     branch
 
-  tracking = "#{tracking_remote}/#{tracking_branch}"
+  # Check the upstream branch exists
+  upstream_branch = "#{tracking_remote}/#{tracking_branch}"
+  failure("Upstream branch '#{upstream_branch}' doesn't exist!") if !repo.exists?(upstream_branch)
 
   head = repo.sha('HEAD')
-  remote = repo.sha(tracking)
+  remote = repo.sha(upstream_branch)
 
   if head == remote
-    puts "Neither your local branch '#{branch}', nor the remote branch '#{tracking}' have moved on."
+    puts "Neither your local branch '#{branch}', nor the remote branch '#{upstream_branch}' have moved on."
     success "Already up-to-date"
+  else
+    merge_base = repo.merge_base(head, remote)
+
+    if merge_base == remote
+      puts "Remote branch '#{upstream_branch}' has not moved on."
+      success "Already up-to-date"
+    else
+      new_commits_on_remote = repo.rev_list(merge_base, remote)
+      is_are, s_or_not = (new_commits_on_remote.length == 1) ? ['is', ''] : ['are', 's']
+      note "There #{is_are} #{new_commits_on_remote.length} new commit#{s_or_not} on master."
+
+      stash_required = repo.dirty?
+      if stash_required
+        repo.stash(:log => true)
+      end
+
+
+    end
+
   end
-
-  merge_base = repo.merge_base(head, remote)
-
-
 end
