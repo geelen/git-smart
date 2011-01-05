9229278e06bb893fa37e386fc0b13daa8422b1fe
Glen Maddern <glenmaddern@gmail.com>
2011-01-05 15:23:22 +1100

    holy documentation batman! Hopefully this will make it pretty easy for people to grok

--- lib/commands/smart-merge.rb
+++ lib/commands/smart-merge.rb
@@ -1,43 +1,63 @@
+#Calling `git smart-merge branchname` will, quite simply, perform a
+#non-fast-forward merge wrapped in a stash push/pop, if that's required.
+#With some helpful extra output.
 GitSmart.register 'smart-merge' do |repo, args|
-  # A smart-merge is quite simply, a non-fast-forward merge with a stash push/pop, if required.
+  #Let's begin!
   current_branch = repo.current_branch
   start "Starting: smart-merge on branch '#{current_branch}'"
 
+  #Grab the merge_target the user specified
   merge_target = args.shift
   failure "Usage: git smart-merge ref" if !merge_target
 
+  #Make sure git can resolve the reference to the merge_target
   merge_sha = repo.sha(merge_target)
   failure "Branch to merge '#{merge_target}' not recognised by git!" if !merge_sha
 
+  #If the SHA of HEAD and the merge_target are the same, we're trying to merge
+  #the same commit with itself. Which is madness!
   head = repo.sha('HEAD')
   if merge_sha == head
     note "Branch '#{merge_target}' has no new commits. Nothing to merge in."
     success 'Already up-to-date.'
   else
+    #Determine the merge-base of the two commits, so we can report some useful output
+    #about how many new commits have been added.
     merge_base = repo.merge_base(head, merge_sha)
 
+    #Report the number of commits on merge_target we're about to merge in.
     new_commits_on_merge_target = repo.rev_list(merge_base, merge_target)
     puts "Branch '#{merge_target}' has diverged by #{new_commits_on_merge_target.length} commit#{'s' if new_commits_on_merge_target.length != 1}. Merging in."
 
+    #Determine if our branch has moved on.
     if head == merge_base
+      #Note: Even though we _can_ fast-forward here, it's a really bad idea since
+      #it results in the disappearance of the branch in history. For a good discussion
+      #on this topic, see this [StackOverflow question](http://stackoverflow.com/questions/2850369/why-uses-git-fast-forward-merging-per-default).
       note "Branch '#{current_branch}' has not moved on since '#{merge_target}' diverged. Running with --no-ff anyway, since a fast-forward is unexpected behaviour."
     else
-      puts "Branch '#{current_branch}' has 1 new commit since '#{merge_target}' diverged."
+      #Report how many commits on our branch since merge_target diverged.
+      new_commits_on_branch = repo.rev_list(merge_base, head)
+      puts "Branch '#{current_branch}' has #{new_commits_on_branch.length} new commit#{'s' if new_commits_on_merge_target.length != 1} since '#{merge_target}' diverged."
     end
 
+    #Before we merge, detect if there are local changes and stash them.
     stash_required = repo.dirty?
     if stash_required
       note "Working directory dirty. Stashing..."
       repo.stash!
     end
 
-    repo.merge!(merge_target)
+    #Perform the merge, using --no-ff.
+    repo.merge_no_ff!(merge_target)
 
+    #If we stashed before, pop now.
     if stash_required
       note "Reapplying local changes..."
       repo.stash_pop!
     end
 
+    #Display a nice completion message in large, friendly letters.
     success "All good. Created merge commit #{repo.sha('HEAD')[0,7]}."
   end
 end
--- lib/commands/smart-pull.rb
+++ lib/commands/smart-pull.rb
@@ -1,13 +1,22 @@
+#Calling `git smart-pull` will fetch remote tracked changes
+#and reapply your work on top of it. It's like a much, much
+#smarter version of `git pull --rebase`.
+#
+#For some background as to why this is needed, see [my blog
+#post about the perils of rebasing merge commits](https://gist.github.com/591209)
+#
+#This is how it works:
+
 GitSmart.register 'smart-pull' do |repo, args|
-  # Let's begin!
+  #Let's begin!
   branch = repo.current_branch
   start "Starting: smart-pull on branch '#{branch}'"
 
-  # Let's not have any arguments, fellas.
+  #Let's not have any arguments, fellas.
   warn "Ignoring arguments: #{args.inspect}" if !args.empty?
 
-  # Try grabbing the tracking remote from the config. If it doesn't exist,
-  # notify the user and choose 'origin'
+  #Try grabbing the tracking remote from the config. If it doesn't exist,
+  #notify the user and default to 'origin'
   tracking_remote = repo.tracking_remote ||
     note("No tracking remote configured, assuming 'origin'") ||
     'origin'
@@ -16,33 +25,45 @@ GitSmart.register 'smart-pull' do |repo, args|
   # but generally that's a good thing. This is the only communication we need to do with the server.
   repo.fetch!(tracking_remote)
 
-  # Try grabbing the tracking branch from the config. If it doesn't exist,
-  # notify the user and choose the branch of the same name
+  #Try grabbing the tracking branch from the config. If it doesn't exist,
+  #notify the user and choose the branch of the same name
   tracking_branch = repo.tracking_branch ||
     note("No tracking branch configured, assuming '#{branch}'") ||
     branch
 
-  # Check the upstream branch exists
+  #Check the specified upstream branch exists. Fail if it doesn't.
   upstream_branch = "#{tracking_remote}/#{tracking_branch}"
   failure("Upstream branch '#{upstream_branch}' doesn't exist!") if !repo.exists?(upstream_branch)
 
+  #Grab the SHAs of the commits we'll be working with.
   head = repo.sha('HEAD')
   remote = repo.sha(upstream_branch)
 
+  #If both HEAD and our upstream_branch resolve to the same SHA, there's nothing to do!
   if head == remote
     puts "Neither your local branch '#{branch}', nor the remote branch '#{upstream_branch}' have moved on."
     success "Already up-to-date"
   else
+    #Find out where the two branches diverged using merge-base. It's what git
+    #uses internally.
     merge_base = repo.merge_base(head, remote)
 
+    #By comparing the merge_base to both HEAD and the remote, we can
+    #determine whether both or only one have moved on.
+    #If the remote hasn't changed, we're already up to date, so there's nothing
+    #to pull.
     if merge_base == remote
       puts "Remote branch '#{upstream_branch}' has not moved on."
       success "Already up-to-date"
     else
+      #If the remote _has_ moved on, we actually have some work to do:
+      #
+      #First, report how many commits are new on remote. Because that's useful information.
       new_commits_on_remote = repo.rev_list(merge_base, remote)
       is_are, s_or_not = (new_commits_on_remote.length == 1) ? ['is', ''] : ['are', 's']
       note "There #{is_are} #{new_commits_on_remote.length} new commit#{s_or_not} on '#{upstream_branch}'."
 
+      #Next, detect if there are local changes and stash them.
       stash_required = repo.dirty?
       if stash_required
         note "Working directory dirty. Stashing..."
@@ -51,21 +72,30 @@ GitSmart.register 'smart-pull' do |repo, args|
 
       success_messages = []
 
+      #Then, bring the local branch up to date.
+      #
+      #If our local branch hasn't moved on, that's easy - we just need to fast-forward.
       if merge_base == head
         puts "Local branch '#{branch}' has not moved on. Fast-forwarding..."
         repo.fast_forward!(upstream_branch)
         success_messages << "Fast forwarded from #{head[0,7]} to #{remote[0,7]}"
       else
+        #If our local branch has new commits, we need to rebase them on top of master.
+        #
+        #When we rebase, we use `git rebase -p`, which attempts to recreate merges
+        #instead of ignoring them. For a description as to why, see my [blog post]((https://gist.github.com/591209).
         note "Both local and remote branches have moved on. Branch 'master' needs to be rebased onto 'origin/master'"
         repo.rebase_preserving_merges!(upstream_branch)
         success_messages << "HEAD moved from #{head[0,7]} to #{repo.sha('HEAD')[0,7]}."
       end
 
+      #If we stashed before, pop now.
       if stash_required
         note "Reapplying local changes..."
         repo.stash_pop!
       end
 
+      #Display a nice completion message in large, friendly letters.
       success ["All good.", *success_messages].join(" ")
     end
 
