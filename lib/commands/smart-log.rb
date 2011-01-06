958b50901890d70b37fdc81dfe91b99dab483dd9
Glen Maddern <glenmaddern@gmail.com>
2011-01-06 14:48:22 +1100

    added ben's even badassier alias, which includes the branch names

--- lib/commands/smart-log.rb
+++ lib/commands/smart-log.rb
@@ -5,5 +5,5 @@
 GitSmart.register 'smart-log' do |repo, args|
   #Super simple, passes the args through to git log, but
   #ratchets up the badassness quotient.
-  repo.log_to_shell('--pretty=format:%Cblue%h%Creset %cr %Cgreen%an%Creset %s', '--graph', *args)
+  repo.log_to_shell('--pretty=format:%Cblue%h%d%Creset %ar %Cgreen%an%Creset %s', '--graph', *args)
 end
